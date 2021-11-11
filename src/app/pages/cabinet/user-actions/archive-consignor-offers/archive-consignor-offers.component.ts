0f22dc3a974482ebfdcc2c9140a5922ff254429d
Oleg Bydlovskiy <olezuk000416@gmail.com>
2021-11-11 23:51:18 +0200

    create archive offers pages

--- src/app/pages/cabinet/user-actions/archive-consignor-offers/archive-consignor-offers.component.ts
+++ src/app/pages/cabinet/user-actions/archive-consignor-offers/archive-consignor-offers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IConsignorOffer } from 'src/app/shared/interfaces/consignor-offer-interface';
+import { ConsignorOffersService } from 'src/app/shared/services/offers/consignor-offers.service';
 
 @Component({
   selector: 'app-archive-consignor-offers',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archive-consignor-offers.component.scss']
 })
 export class ArchiveConsignorOffersComponent implements OnInit {
-
-  constructor() { }
+  public activeOffersList !: IConsignorOffer[];
+  private currentUserId = JSON.parse(localStorage.getItem('user') as string).id;
+  constructor(private consignorOfferService: ConsignorOffersService) { }
 
   ngOnInit(): void {
+    this.loadOfferList();
+  }
+
+  loadOfferList(): void {
+    this.consignorOfferService.getAllforCurrentUser(this.currentUserId).then(data => {
+      this.activeOffersList = [];
+      let offerList: any[] = [];
+      data.forEach(offer => {
+        offerList.push(offer.data() as IConsignorOffer);
+      });
+      this.activeOffersList = offerList.filter(offer => offer.status == "archive");
+      console.log(this.activeOffersList);
+    })
   }
 
 }
